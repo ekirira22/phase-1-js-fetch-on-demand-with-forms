fb9d939c8226d14a2cb870f8be1d2c8efbb01ac2
Eric Maranga <ekirira22@gmail.com>
2024-01-06 20:03:23 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 const init = () => {
-  
+    /******************** Event Listeners ********************/
+    document.querySelector('form').addEventListener('submit', e => {
+        e.preventDefault()
+        retrieveMovies(e)
+        e
+    })
+
+
+    /******************** Functions ********************/
+
+    function retrieveMovies(e){
+        const value = e.target.children[1].value
+        fetch(`http://127.0.0.1:3000/movies/${value}`)
+            .then(resolve => resolve.json())
+                .then(data => displayResult(data))
+    }
+
+    function displayResult(movie){
+        const title = document.querySelector('section#movieDetails h4')
+        const summary = document.querySelector('section#movieDetails p')
+        
+        title.textContent = movie.title
+        summary.textContent = movie.summary
+        
+    }
+
+   
 }
 
 document.addEventListener('DOMContentLoaded', init);
